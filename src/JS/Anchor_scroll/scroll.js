a6a48d5b81cf7ddaf96351eddae10ebba77a1f5d
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2022-05-04 09:39:57 +0300

    Update scroll.js

--- src/JS/Anchor_scroll/scroll.js
+++ src/JS/Anchor_scroll/scroll.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
   $('#navigation').on('click', 'a', function (event) {
     event.preventDefault();
-    var id = $(this).attr('href'),
+    let id = $(this).attr('href'),
       top = $(id).offset().top;
     $('body,html').animate({ scrollTop: top }, 1500);
   });
